104bd30e781036f74ce4a83b247f0ed6bd9a656d
ingrid <ingrid7613@gmail.com>
2022-06-13 15:10:58 +0800

    新增TDX-BikeStation：2. 使用API金鑰

--- src/views/BikeListView.vue
+++ src/views/BikeListView.vue
@@ -41,8 +41,7 @@ export default {
         '$format' : 'JSON',
         //取前幾筆
         // '$top': 100
-      },
-      total_pages: 0
+      }
     }
   },
   components: {
@@ -50,24 +49,48 @@ export default {
     RegresAllBike
   },
   mounted(){
-    this.AxiosFunc()
+    this.getAuthorizationHeader().then((accesstoken)=>{
+      if(!accesstoken) return
+      this.AxiosFunc(accesstoken)
+    })
   },
   methods: {
-    AxiosFunc(){
+    getAuthorizationHeader(){
+      //https://github.com/tdxmotc/SampleCode
+      //方法一：用URLSearchParams
+      const AUTH_URL = "https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token"
+      const parameter = new URLSearchParams()
+      parameter.append('grant_type', 'client_credentials')
+      parameter.append('client_id', 'ingrid7613-b0b80d35-76c6-41fd')
+      parameter.append('client_secret', '3808da7e-51be-4ff1-bb66-d03f61b11b96')
+      //方法二：用qs，需要安裝qs函式庫
+
+      return axios({
+        method: "POST",
+        url: AUTH_URL,
+        headers: { "content-type": "application/x-www-form-urlencoded" },   
+        data: parameter
+      }).then((response) => {
+        //把response.data 丟給 Line69，再透過70行return到55行
+        return response.data
+      }).catch((err) => {
+        return err
+      })
+    },
+    AxiosFunc(accesstoken){
       this.axioAllData = []
       //取得指定[縣市]的公共自行車租借站位資料
       axios.get(`${API_URL}v2/Bike/Station/${this.city}`, {
+        headers: {
+          authorization: `Bearer ${accesstoken.access_token}`,
+        },
         params: this.params
       }).then((response) => {
         this.axioError = (response.status !== 200)
-        // this.total_pages = response.data.total_pages
         this.axioAllData = response.data
       }).catch((err) => {
         this.axioError = true
       })
-    },
-    selectPage(page){
-      this.AxiosFunc()
     }
   }
 }
