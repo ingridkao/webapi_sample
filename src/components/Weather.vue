c90a625222069ac42479cecb6ccace5a5b5b86a3
ingrid <ingrid7613@gmail.com>
2022-06-13 19:03:51 +0800

    新增氣象觀測:2. 初步串接API

---
+++ src/components/Weather.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div v-if="error">
+      Error
+    </div>
+    <div v-else-if="allData.length === 0">
+      No Data
+    </div>
+    <table v-else class="userBox">
+      <th v-for="(item, index) in title" :key="index">
+        <!-- Vue3後，Object可以被自動解析不用用Object.values -->
+        {{item}}
+      </th>
+      <tr v-for="item in allData" :key="item.locationName">
+        <td>{{item.locationName}}</td>
+        <td v-for="(el, index) in item.weatherElement" :key="index">
+          <div v-for="(weather, index2) in el.time" :key="index2">
+            <!-- {{weather.startTime}}-{{weather.endTime}} -->
+            {{weather.parameter.parameterName}}{{weather.parameter.parameterUnit}}
+          </div>
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: Boolean,
+    allData: Array
+  },
+  data(){
+    return{
+      title: {
+        locationName: '縣市',
+        weatherElement: '天氣'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/views/WeatherView.vue
+++ src/views/WeatherView.vue
@@ -2,15 +2,10 @@
   <div>
     <div>
       <HelloWorld 
-        msg="使用氣象資料開放平臺取得觀測資料"
-        link="https://opendata.cwb.gov.tw/index"
+        msg="使用氣象資料開放平臺取得觀測資料-一般天氣預報-今明 36 小時天氣預報"
+        link="https://opendata.cwb.gov.tw/dataset/observation/O-A0003-001"
       />
-      <div>
-        <ol>
-          <li>必填：</li>
-        </ol>
-      </div>
-      <RegresAllBike 
+      <Weather 
         :error="axioError"
         :allData="axioAllData"
       />
@@ -20,31 +15,43 @@
 
 <script>
 import HelloWorld from '@/components/HelloWorld.vue'
-import RegresAllBike from '@/components/RegresAllBike.vue'
+import Weather from '@/components/Weather.vue'
 import axios from 'axios'
-const API_URL = 'https://tdx.transportdata.tw/api/basic/'
+//※ URL：https://opendata.cwb.gov.tw/api/v1/rest/datastore/{dataid}?Authorization={apikey}&format={format}
+const API_URL = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/'
 export default {
   data(){
     return {
       axioError: false,
       axioAllData: [],
-      params:{},
+      params:{
+        'Authorization': 'CWB-DBEF296D-42A4-4C58-8CC7-4D51E3C1E878',
+        // 'locationName': ['宜蘭縣'],
+        'elementName': ['Wx','MaxT','MinT','PoP']
+      }
     }
   },
   components: {
     HelloWorld,
-    RegresAllBike
+    Weather
   },
   mounted(){
     this.AxiosFunc()
   },
   methods: {
     AxiosFunc(){
-      axios.get(``, {
-        params: this.params
+      //https://opendata.cwb.gov.tw/opendatadoc/MFC/ForecastElement.pdf
+      //elementName可以篩選
+      //官網上說是array[string]，但測試後發現他是以逗號隔開
+      axios.get(`${API_URL}F-C0032-001`, {
+        params: {
+          ...this.params,
+          // 'locationName': this.params.locationName.toString(),
+          'elementName': this.params.elementName.toString()
+        }
       }).then((response) => {
         this.axioError = (response.status !== 200)
-        this.axioAllData = response.data
+        this.axioAllData = response.data.records.location
       }).catch((err) => {
         this.axioError = true
       })
