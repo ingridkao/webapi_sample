d25a50b84ca35ed571c9690afb17090a452776ca
ingrid <ingrid7613@gmail.com>
2022-06-13 13:41:23 +0800

    新增search ID功能

---
+++ src/components/RegresTargetUser.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div v-if="error">
+      Error
+    </div>
+    <div v-else-if="targetData && Object.keys(targetData).length === 0">
+      No Users
+    </div>
+    <div v-else class="userBox">
+      {{targetData}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: Boolean,
+    targetData: Object || null
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.userBox {
+  min-height: 25vh;
+  >div{
+    display: inline-block;
+    vertical-align: top;
+    text-align: center;
+    margin: 5px 10px;
+    p{
+      margin: 5px;
+    }
+    .imgBox{
+      width: 128px;
+      height: 128px;
+      margin: auto;
+      border-radius: 50%;
+      overflow: hidden;
+      img{
+        width: 100%;
+      }
+    }
+  }
+}
+</style>
--- src/views/ReqresView.vue
+++ src/views/ReqresView.vue
@@ -6,17 +6,18 @@
         link="https://reqres.in/"
       />
       <h6>Get all user: response是陣列[]</h6>
+      <p>一般來說search應該要用這一個API來做，但是API沒有提供search功能，所以用target user來示意</p>
       <RegresAllUser 
         :XMLHttpError="axioError"
         :userAllData="axioAllData"
       />
       <h6>Get traget user: response是物件{}</h6>
-      <div v-if="axioTargetError">
-        Error
-      </div>
-      <div v-else>
-        {{axioTargetData? axioTargetData: 'No Users'}}
-      </div>
+      <label for="search">Search ID</label>
+      <input type="text" id="search" v-model="search" @keyup="searchUser">
+      <RegresTargetUser 
+        :error="axioTargetError"
+        :targetData="axioTargetData"
+      />
     </div>
   </div>
 </template>
@@ -24,6 +25,7 @@
 <script>
 import HelloWorld from '@/components/HelloWorld.vue'
 import RegresAllUser from '@/components/RegresAllUser.vue'
+import RegresTargetUser from '@/components/RegresTargetUser.vue'
 import axios from 'axios'
 export default {
   data(){
@@ -31,12 +33,14 @@ export default {
       axioError: false,
       axioAllData: [],
       axioTargetError: false,
-      axioTargetData: {}
+      axioTargetData: {},
+      search: ''
     }
   },
   components: {
     HelloWorld,
-    RegresAllUser
+    RegresAllUser,
+    RegresTargetUser
   },
   mounted(){
     this.AxiosFunc()
@@ -52,13 +56,19 @@ export default {
       })
     },
     AxiosTargetFunc(){
-      axios.get('https://reqres.in/api/users/2').then((response) => {
+      //增加判斷
+      this.axioTargetData = {}
+      if(this.search === "")return
+      axios.get(`https://reqres.in/api/users/${this.search}`).then((response) => {
         this.axioTargetError = (response.status !== 200)
         this.axioTargetData = response.data.data
       }).catch((err) => {
         this.axioTargetError = true
       })
-    } 
+    },
+    searchUser(){
+      this.AxiosTargetFunc()
+    }
   }
 }
 </script>
