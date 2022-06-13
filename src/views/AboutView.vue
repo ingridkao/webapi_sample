154a80d5d5c9289a7e909219b02d18f63e756ee6
ingrid <ingrid7613@gmail.com>
2022-06-13 11:40:28 +0800

    使用XMLHttpRequest取得API

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,64 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <div>
+      <HelloWorld 
+        msg="使用Reqres來試試看Get user"
+        link="https://reqres.in/"
+      />
+      <h6>
+        1. XMLHttpRequest: JavaScript的原生物件，不需引入任何函式庫
+      </h6>
+      <div v-if="XMLHttpError">
+        Error
+      </div>
+      <div v-else>
+        {{userAllData.length > 0 ? userAllData: 'No Users'}}
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  data(){
+    return {
+      XMLHttpError: false,
+      userAllData: []
+    }
+  },
+  components: {
+    HelloWorld
+  },
+  created(){
+    // created -> el掛載完成，整個view已被渲染完成 -> mounted
+    // 當網速慢或是專案目錄過大的時候，放在created或mounted會有差異
+    // console.log('created');
+  },
+  mounted(){
+    this.XMLHttpRequestFunc()
+  },
+  methods: {
+    XMLHttpRequestFunc(){
+      const xhr = new XMLHttpRequest()
+
+      // 初始化一個請求: (method, url, 同步｜非同步）
+      xhr.open('GET', 'https://reqres.in/api/users', true)
+      xhr.send()
+
+      // 要用"箭頭函式"，用下面的寫法作用域裡面的this不是Vue
+      // xhr.onload = function(){}
+      xhr.onload = () => { 
+        const useData = JSON.parse(xhr.response)
+        console.log(useData);
+        this.userAllData = useData.data
+      }
+      xhr.onerror = function (err) {
+        this.XMLHttpError = true
+        console.log('錯誤:', err)
+      }
+    }
+  }
+}
+</script>
